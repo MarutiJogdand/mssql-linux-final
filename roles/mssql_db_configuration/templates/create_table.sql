decb83355603a2e3f421a6e00f0d5a825a5d141c
Cloud User <ec2-user@ip-172-31-27-113.eu-west-1.compute.internal>
2018-11-22 11:52:28 +0000

    maruti's Commit

--- roles/mssql_db_configuration/templates/create_table.sql
+++ roles/mssql_db_configuration/templates/create_table.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE {{database_name}}
+GO
 
-use {{database_name}}
-go
+USE {{database_name}}
+GO
 
 CREATE TABLE {{table_name}}(ID int PRIMARY KEY NOT NULL,Name varchar(25) NOT NULL,Description text NULL)  
 GO  
